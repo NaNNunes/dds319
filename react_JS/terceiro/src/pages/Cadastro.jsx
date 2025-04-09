fe401cd6982d2cc8b1baf317a69b76c8aa672e14
Renan Nunes <renan.s.nunes@aluno.senai.br>
2025-04-08 21:38:31 -0300

    bootstrap e front ezisses

--- react_JS/terceiro/src/pages/Cadastro.jsx
+++ react_JS/terceiro/src/pages/Cadastro.jsx
@@ -1,28 +1,35 @@
 import FloatingLabel from 'react-bootstrap/FloatingLabel';
 import Form from 'react-bootstrap/Form';
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
 import Button from 'react-bootstrap/Button'
 const Cadastro = () => {
   return (
     <div>
-      <FloatingLabel
-        controlId="floatingNome"
-        label="Nome"
-        className="mb-3"
-      >
-        <Form.Control type="text" placeholder='' size='lg'/>
-      </FloatingLabel>
-
-      <FloatingLabel 
-        controlId="floatingInput"
-        label="E-mail"
-        className="mb-3"
-      >
-        <Form.Control type="e-mail" placeholder=''/>
-      </FloatingLabel>
-
-      <FloatingLabel controlId="floatingPassword" label="Password">
-        <Form.Control type="password" placeholder="" />
-      </FloatingLabel>
+        <Row className='mt-3'>
+          <Col>
+            <FloatingLabel
+              controlId="floatingNome"
+              label="Nome"
+              className="mb-3"
+            >
+              <Form.Control type="text" placeholder='' size='lg'/>
+            </FloatingLabel>
+          </Col>
+        
+          <Col>
+            <FloatingLabel
+              controlId="floatingInput"
+              label="E-mail"
+              className="mb-3"
+            >
+              <Form.Control type="e-mail" placeholder=''/>
+            </FloatingLabel>
+          </Col>
+        </Row>
+        <FloatingLabel controlId="floatingPassword" label="Password">
+              <Form.Control type="password" placeholder="" />
+            </FloatingLabel>
 
       <Form.Group controlId="formFile" className="mb-3">
         <Form.Label>Default file input example</Form.Label>
@@ -32,6 +39,13 @@ const Cadastro = () => {
         <Form.Label>Multiple files input example</Form.Label>
         <Form.Control type="file" multiple />
       </Form.Group>
+
+      <Form.Select aria-label="Default select example">
+        <option>Selecione algo</option>
+        <option value="adm">ADM</option>
+        <option value="op">Operador</option>
+        <option value="solicitante">Solicitante</option>
+    </Form.Select>
 
       <Button variant="dark" className='mt-3'>Cadastrar</Button>
     </div>
