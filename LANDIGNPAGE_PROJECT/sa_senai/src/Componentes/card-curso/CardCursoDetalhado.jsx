543ef75139254420387b30e3de7fc76b4ace749a
Renan Nunes <renan.s.nunes@aluno.senai.br>
2025-05-14 09:30:52 -0300

    gambiarra

---
+++ LANDIGNPAGE_PROJECT/sa_senai/src/Componentes/card-curso/CardCursoDetalhado.jsx
@@ -0,0 +1,79 @@
+import Container from 'react-bootstrap/Container'
+import Card from 'react-bootstrap/Card'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+import Button from 'react-bootstrap/Button'
+
+import { Link } from 'react-router-dom'
+
+import urlImg from "../../assets/ddsimg.png";
+
+import styles from "./CursoCard.module.css";
+
+const CardCursoDetalhado = () => {
+  return (
+    <div>
+        <Container className={styles.container}>
+            <Card className={styles.cardcontainer} style={{marginTop:"1rem"}}>
+                <Card.Header className={styles.cardheader}>
+                    <Card.Title className={styles.name}>
+                        Titulo
+                    </Card.Title>
+                    <Row
+                        className={styles.cursoimg}
+                        style={{background:`no-repeat center/100% url(${urlImg})`}}
+                    />
+                </Card.Header>
+
+                <Card.Body className='text-center'>
+                   
+                    <Row>
+                        
+                        <Col xl={8} lg={10} md={7} sm={10} xs={10} 
+                         className='m-auto d-flex flex-wrap justify-content-around'>
+                            
+                                <div className={styles.localidade}>
+                                    localidade
+                                </div>
+                                <div className={styles.duracao}>
+                                    duracao
+                                </div>
+                                <div className={styles.duracao}>
+                                    Turno
+                                </div>
+                                <div className={styles.modalidade}>
+                                    Modalidade
+                                </div>
+                                <div className={styles.data}>Data</div>
+                                <div className={styles.valortotalcontainer}>
+                                    R$ 2.994,00
+                                </div>
+                            
+                        </Col>
+                                    
+                        
+                        
+                        <Row>
+                            <Col className={styles.parcela}>até 6x de R$ 499,00</Col>
+                        </Row>
+                    </Row>
+
+                    <Row>
+                        <Col>
+                            <Button
+                                as='input'
+                                value={"Matricule-se"}
+                                type='submit'
+                                size='lg'
+                                className={styles.btn}
+                            />
+                        </Col>
+                    </Row>
+                </Card.Body>
+            </Card>
+        </Container>
+    </div>
+  )
+}
+
+export default CardCursoDetalhado
--- LANDIGNPAGE_PROJECT/sa_senai/src/pages/DevSys.jsx
+++ LANDIGNPAGE_PROJECT/sa_senai/src/pages/DevSys.jsx
@@ -3,55 +3,23 @@ import React from 'react'
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
-import CursoObjetivo from '../Componentes/curso-componentes/CursoObjetivo.jsx';
-import PreRequisitos from '../Componentes/curso-componentes/PreRequisitos.jsx';
-import Documentos from '../Componentes/curso-componentes/Documentos.jsx';
+import ObjetivoDocumentos from '../Componentes/curso-componentes/ObjetivoDocumentos.jsx';
+import PreRequisitosConteudo from '../Componentes/curso-componentes/PreRequisitosConteudo.jsx';
+
+import CardCursoDetalhado from '../Componentes/card-curso/CardCursoDetalhado.jsx';
 
 import ddsImg from "../assets/ddsimg.png";
 
 const DevSys = () => {
   return (
     <Container>
-      <Row>
-        <Col lg={6} md={12}>
-          <Container
-            style={
-                {
-                  marginTop:"1rem",
-                  height:"400px",
-                  background:`bottom/100% url(${ddsImg})`,
-                  borderRadius:"10px",
-                  boxShadow:"2px 3px 4px black"
-                }
-            }
-          >
-            <Row
-                className="fs-3 fw-bold text-center text-uppercase rounded-top-2"
-                style={
-                    {
-                      padding:"1rem 1rem 1.5rem",
-                      marginBottom:"1rem",
-                      color:"white",
-                      background:"linear-gradient(to right, #FC5707, #51199E)"
-                    }
-                }
-            >
-              <Col>
-                Técnico em Desenvolvimento de Sistemas
-              </Col>
-            </Row>
-            <Col>
-                <CursoObjetivo  />
-            </Col>
-            <Col >
-                  <PreRequisitos/>
-            </Col>
-          </Container>
+      <Row className='d-flex flex-row-reverse'>
+        <Col lg={6} xs={12}>
+          <ObjetivoDocumentos/>
         </Col>
-        <Col>
-          <Container>
-            <Documentos/>
-          </Container>
+        <Col lg={6} xs={12}>
+            <CardCursoDetalhado/>
+            <PreRequisitosConteudo/>
         </Col>
       </Row>
     </Container>
